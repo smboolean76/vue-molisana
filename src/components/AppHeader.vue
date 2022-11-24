c18a3d0a5da5664af53e8d814a6721b2e03bb8b7
smboolean76 <smboolean76@madrigali.dev>
2022-11-24 13:07:06 +0100

    Header

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -1,12 +1,55 @@
 <script>
+import AppLogo from './AppLogo.vue';
+
 export default {
   name: 'AppHeader',
+  components: {
+    AppLogo
+  },
+  data() {
+    return {
+        links: [
+            'Home',
+            'Prodotti',
+            'Chi Siamo',
+            'Contatti'
+        ]
+    }
+  }
 }
 </script>
 
 <template>
-  <h1>Header</h1>
+    <header class="container">
+        <AppLogo/>
+        <nav class="main-nav">
+            <ul>
+                <li v-for="link in links"><a href="">{{link}}</a></li>
+            </ul>
+        </nav>
+    </header>
 </template>
 
-<style>
+<style lang="scss" scoped>
+header {
+    text-align: center;
+    .main-nav {
+        ul {
+            display: flex;
+            list-style: none;
+            justify-content: center;
+
+            a {
+                display: inline-block;
+                padding: 15px 25px;
+                text-decoration: none;
+                color: #000;
+
+                &.active {
+                    background-color: var(--secondary-color);
+                }
+            }
+        }
+    }
+}
 </style>
---
+++ src/components/AppLogo.vue
@@ -0,0 +1,12 @@
+<script>
+export default {
+  name: 'AppLogo',
+}
+</script>
+
+<template>
+  <img src="../assets/la-molisana-logo.png" alt="Logo">
+</template>
+
+<style>
+</style>
